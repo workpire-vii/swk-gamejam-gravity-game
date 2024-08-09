341de3c384bdb60ad58b384a1163c5be0ee9f586
SleepyZhi <laidezhi317@gmail.com>
2024-08-09 19:04:46 +0800

    Adding TexMeshPro

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,27 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float thrustSpeed = 1;
+    [SerializeField] private TextMeshProUGUI scoreSpeed;
+    [NonSerialized] public float speed;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-
     void Update()
     {
-        
+        updateScore();
     }
+
     private void FixedUpdate()
     {
         momentumHandling();
     }
 
+    private void updateScore()
+    {
+        speed = Vector2.SqrMagnitude(rb.velocity);
+        scoreSpeed.text = speed.ToString();
+    }
     private void momentumHandling()
     {
         if (Input.GetKey(KeyCode.Mouse0))
@@ -29,6 +38,5 @@ public class PlayerMovement : MonoBehaviour
             Vector2 mousePos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
             rb.AddForce((-mousePos) * thrustSpeed);
         }
-
     }
 }
